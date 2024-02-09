3dd88a1599e0409a956ceede3203f6dd02788b6a
Kunal Chand <kunalchand234@gmail.com>
2024-02-09 18:00:37 -0500

    1043. Partition Array for Maximum Sum O(n*k)

--- kunal/1043. Partition Array for Maximum Sum.py
+++ kunal/1043. Partition Array for Maximum Sum.py
@@ -1,35 +1,27 @@
 class Solution:
-    def findMax(self, arr, start, end) -> int:
-        max_ = 0
+    def maxSum(self, dp, arr, start, end, k) -> int:
+        maxValue = float("-inf")
+        maximumSum = float("-inf")
 
-        for i in range(start, end + 1):
-            max_ = max(max_, arr[i])
+        while start >= 0 and (end - start + 1) <= k:
+            maxValue = max(maxValue, arr[start])
+            maximumSum = max(maximumSum, dp[start] + ((end - start + 1) * maxValue))
+            start -= 1
 
-        return max_
+        return maximumSum
 
     def maxSumAfterPartitioning(self, arr: list[int], k: int) -> int:
         dp = []
         dp.append(0)
-        # dp = [0 for i in range(len(arr) + 1)]
 
         for i in range(len(arr)):
-            # exclude a[i] from the ongoing subarray
-            exclude = arr[i] + dp[i]
+            # exclude arr[i] from the ongoing subarray
+            exclude = dp[i] + arr[i]
 
-            # include a[i] in the ongoing subarray
-            start = i - k + 1 if i - k + 1 >= 0 else 0
-            end = i
-
-            include = 0
-            while start <= end:
-                include = max(
-                    include,
-                    dp[start] + ((end - start + 1) * self.findMax(arr, start, end)),
-                )
-                start += 1
+            # include arr[i] in the ongoing subarray
+            include = self.maxSum(dp, arr, i, i, k)
 
             dp.append(max(exclude, include))
-            # dp[i + 1] = max(exclude, include)
 
         return dp[-1]
 
