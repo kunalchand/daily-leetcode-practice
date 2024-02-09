615840baf0792b82495b705e0fd1139cbf8060e4
Rishabh <rishabhvrm43@gmail.com>
2024-02-09 15:37:13 -0500

    refactor code

--- rishabh/2966. Divide Array Into Arrays With Max Difference.py
+++ rishabh/2966. Divide Array Into Arrays With Max Difference.py
@@ -1,23 +1,43 @@
 from typing import List
 class Solution:
     def divideArray(self, nums: List[int], k: int) -> List[List[int]]:
-        nums.sort()
         
-        # make sub arrays
+        # Approach: sort
+        # make sub arrays of size 3 and append to res
+        # if it's an invalid subarray (diff > k), then return []
+        # TIME: O(n * log n)
+        '''
+        nums.sort()
         res = []
         i, l = 0, len(nums)
 
         while i < l:
             count = 0
-            curr_window = []
-            # make a window of 3
-            while i < l and count < 3:
+            curr_window = []                    # sub array
+            
+            while i < l and count < 3:          # make a window of size 3
                 curr_window.append(nums[i])
                 i += 1
                 count += 1
             
             if abs(curr_window[0] - curr_window[2]) > k: return []
             res.append(curr_window)
-            # i += 1
-
+            
         return res
+        '''
+        # same code as above but cleaner
+        nums.sort()
+        res = []
+
+        for i in range(0, len(nums), 3):
+            if nums[i + 2] - nums[i] > k: return []                 # return if sub array is invalid
+            res.append([nums[i], nums[i + 1], nums[i + 2]])
+        
+        return res
+    
+
+
+obj = Solution()
+nums = [1,3,4,8,7,9,3,5,1]
+k = 2
+print(obj.divideArray())
