928e3efd09603e9f9397cd0d4425a26abfadb121
Rishabh <rishabhvrm43@gmail.com>
2024-02-11 03:11:19 -0500

    add another approach

--- rishabh/647. Palindromic Substrings.py
+++ rishabh/647. Palindromic Substrings.py
@@ -1,18 +1,43 @@
 class Solution:
+    '''
+    APPROACH-1: Brute force
+    Check every possible sub-string from starting to end
+    TIME: O(N ^ 3)
+    SPACE: O(1)
+    '''
     def countSubstrings(self, s: str) -> int:
-        '''
-        APPROACH: Brute force
-        Check every possible sub-string from starting to end
-        TIME: O(N ^ 3)
-        SPACE: O(1)
-        '''
-        def is_palindrome(sbs):
+        def is_palindrome(sbs):                                             # T(N) = O (N)
             return sbs == sbs[::-1]
         res = []
-        for i in range(len(s)):
-            for j in range(i+1, len(s)):
+        # T(N) = O (N ^ 3)
+        for i in range(len(s)):                                             # T(N) = O (N)
+            for j in range(i+1, len(s)):                                    # T(N) = O (N)
                 substring = s[i : j + 1]
-                if(is_palindrome(substring)): res.append(substring)
+                if(is_palindrome(substring)): res.append(substring)         # T(N) = O (N)
         print(res)
         return len(s) + len(res)
+     
         
+
+    '''
+    APPROACH-2: expand around every possible center
+    odd length (single char center) and 
+    even length palindromes (consecutive char center)
+    TIME: O(N ^ 2)
+    SPACE: O(1)
+    '''    
+    def countSubstrings(self, s: str) -> int:
+        # T(N) = O(N)
+        def count_palindrome(left, right):
+            curr_count = 0
+            while 0 <= left and right < len(s) and s[left] == s[right]:
+                curr_count += 1
+                left, right = left - 1, right + 1
+            return curr_count
+
+        ans = 0
+        # T(N) = O(N ^ 2)
+        for i in range(len(s)):                      # T(N) = O(N)
+            ans += count_palindrome(i, i)            # odd length (single char center)
+            ans += count_palindrome(i, i + 1)        # even length palindromes (consecutive char center)
+        return ans
