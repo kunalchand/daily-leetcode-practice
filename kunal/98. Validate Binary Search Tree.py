d909e09ec21031081a1b2aa8b46a4737db20158b
Kunal Chand <kunalchand234@gmail.com>
2024-02-15 11:42:31 -0500

    O(n) Space approach

--- kunal/98. Validate Binary Search Tree.py
+++ kunal/98. Validate Binary Search Tree.py
@@ -10,6 +10,8 @@ class TreeNode:
 
 
 class Solution:
+    # Time-O(n) Space-O(1) Validate Left and Right Sub-Tree
+    """
     def getMaxMin(self, root: Optional[TreeNode]) -> Tuple[int, int, bool]:
         if root == None:
             return (float("-inf"), float("inf"), True)
@@ -26,3 +28,22 @@ class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
         root_max, root_min, root_bool = self.getMaxMin(root)
         return root_bool
+    """
+
+    # Time-O(n) Space-O(n) In-Order Traversal
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        list_ = []
+
+        def inOrderTraversal(root: Optional[TreeNode]) -> None:
+            if root:
+                inOrderTraversal(root.left)
+                list_.append(root.val)
+                inOrderTraversal(root.right)
+
+        inOrderTraversal(root)
+
+        for i in range(1, len(list_)):
+            if list_[i] <= list_[i - 1]:
+                return False
+
+        return True
