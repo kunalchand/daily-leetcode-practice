04ae1ca64ee36d921874690381b57e10046da7e6
Kunal Chand <kunalchand234@gmail.com>
2024-03-31 04:36:30 -0400

    49. Group Anagrams (Updated Code)

--- kunal/49. Group Anagrams.py
+++ kunal/49. Group Anagrams.py
@@ -1,21 +1,41 @@
-from collections import defaultdict
-from typing import List
+import copy
+import heapq
+import math
+import random
+from collections import Counter, defaultdict, deque
+from dataclasses import dataclass
+from functools import cmp_to_key, reduce
+from itertools import zip_longest
+from math import factorial, sqrt
+from typing import Dict, List, Optional, Set, Tuple, Union
 
 
 class Solution:
+    # Sorted String as Key Time-O(n*k*logk) Space-O(n*k) n=strs.length, k=max(strs[i].length)
+    """
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        dict_ = defaultdict(list)
+        buckets = defaultdict(list)
 
         for string in strs:
             sorted_string = "".join(sorted(string))
-            dict_[sorted_string].append(string)
+            buckets[sorted_string].append(string)
 
-        ans = []
+        return buckets.values()
+    """
 
-        for value_list in dict_.values():
-            ans.append(value_list)
+    # Character Count Time-O(n*k) Space-O(n*26) n=strs.length, k=max(strs[i].length)
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        buckets = defaultdict(list)
 
-        return ans
+        for str_ in strs:
+            count = [0] * 26
+            for char in str_:
+                count[ord(char) - ord("a")] += 1
+            buckets[tuple(count)].append(str_)
+
+        return buckets.values()
 
 
 print(Solution().groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]))
+print(Solution().groupAnagrams([""]))
+print(Solution().groupAnagrams(["a"]))
