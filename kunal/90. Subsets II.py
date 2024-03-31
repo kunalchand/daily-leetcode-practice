bd21aad57bad8140e55f1cec9e10702c2cf8b111
Kunal Chand <kunalchand234@gmail.com>
2024-03-31 06:26:01 -0400

    90. Subsets II (Code Update)

--- kunal/90. Subsets II.py
+++ kunal/90. Subsets II.py
@@ -1,36 +1,57 @@
-from typing import List
+import bisect
+import copy
+import heapq
+import math
+import random
+from bisect import bisect, bisect_left, bisect_right, insort, insort_left, insort_right
+from collections import Counter, defaultdict, deque
+from dataclasses import dataclass
+from functools import cmp_to_key, reduce
+from itertools import zip_longest
+from math import ceil, factorial, floor, sqrt
+from typing import Dict, List, Optional, Set, Tuple, Union
 
 
+# https://leetcode.com/problems/subsets-ii/description/
 class Solution:
-    def __init__(self):
-        self.hashset = set()
+    # Set (TLE Approach) (Won't give TLE just because of the constraints)
+    """
+    def generateSubsets(self, nums: List[int], current: List[int]) -> None:
+        if tuple(sorted(current)) not in self.duplicates:
+            self.subsets.append(current)
+            self.duplicates.add(tuple(sorted(current)))
 
-    def helper(self, ongoing: List[int], addon: List[int]) -> None:
-        self.hashset.add(tuple(ongoing))
-
-        if len(addon) == 0:
-            return
-        else:
-            for index, item in enumerate(addon):
-                self.helper(ongoing + [item], addon[index + 1 :])
+        for index, num in enumerate(nums):
+            self.generateSubsets(nums[index+1:], current + [num])
 
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        # Tuple Order in set matter. (1,4) and (4,1) have different hash
+        self.subsets = []
+        self.duplicates = set()
+
+        # Size 0 or more
+        for num in nums:
+            self.generateSubsets(nums, [])
+
+        return self.subsets
+    """
+
+    # Sort
+    def generateSubsets(self, nums: List[int], current: List[int]) -> None:
+        self.subsets.append(current)
+
+        for index, num in enumerate(nums):
+            if index == 0 or nums[index - 1] != nums[index]:
+                self.generateSubsets(nums[index + 1 :], current + [num])
+
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
 
-        # Subset Size 0
-        self.hashset.add(())
+        self.subsets = []
 
-        # Subset Size 1 and more
-        for index, item in enumerate(nums):
-            self.helper([item], nums[index + 1 :])
+        # Size 0 or more
+        self.generateSubsets(nums, [])
 
-        ans = []
-
-        for element in self.hashset:
-            ans.append(list(element))
-
-        return ans
+        return self.subsets
 
 
 print(Solution().subsetsWithDup([1, 2, 2]))
