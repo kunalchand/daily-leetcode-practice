8ad3a5beffe746fe536f540ed83a4de2078704de
Kunal Chand <kunalchand234@gmail.com>
2024-03-31 05:32:50 -0400

    39. Combination Sum (Code Update)

--- kunal/39. Combination Sum.py
+++ kunal/39. Combination Sum.py
@@ -1,21 +1,39 @@
-from typing import List
+import copy
+import heapq
+import math
+import random
+from collections import Counter, defaultdict, deque
+from dataclasses import dataclass
+from functools import cmp_to_key, reduce
+from itertools import zip_longest
+from math import factorial, sqrt
+from typing import Dict, List, Optional, Set, Tuple, Union
 
 
+# https://leetcode.com/problems/combination-sum/
 class Solution:
-    def __init__(self):
-        self.ans = []
-
-    def helper(self, ongoing: List[int], addon: List[int], target: int) -> None:
-        if sum(ongoing) > target:
+    def generateCombination(
+        self, candidates: List[int], target: int, current: List[int]
+    ) -> None:
+        if sum(current) == target:
+            self.ans.append(current)
             return
-        elif sum(ongoing) == target:
-            self.ans.append(ongoing)
+        elif sum(current) > target:
+            return
         else:
-            for index, item in enumerate(addon):
-                self.helper(ongoing + [item], addon[index:], target)
+            for index, candidate in enumerate(candidates):
+                self.generateCombination(
+                    candidates[index:], target, current + [candidate]
+                )
 
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        for index, item in enumerate(candidates):
-            self.helper([item], candidates[index:], target)
+        self.ans = []
+
+        self.generateCombination(candidates, target, [])
 
         return self.ans
+
+
+print(Solution().combinationSum([2, 3, 6, 7], 7))
+print(Solution().combinationSum([2, 3, 5], 8))
+print(Solution().combinationSum([2], 1))
