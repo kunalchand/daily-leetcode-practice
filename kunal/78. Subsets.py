5ab980fb535e26ea2dcbe96de9b07d7f57d3756f
Kunal Chand <kunalchand234@gmail.com>
2024-03-31 05:30:20 -0400

    78. Subsets (Code Update)

--- kunal/78. Subsets.py
+++ kunal/78. Subsets.py
@@ -1,27 +1,30 @@
-from typing import List
+import copy
+import heapq
+import math
+import random
+from collections import Counter, defaultdict, deque
+from dataclasses import dataclass
+from functools import cmp_to_key, reduce
+from itertools import zip_longest
+from math import factorial, sqrt
+from typing import Dict, List, Optional, Set, Tuple, Union
 
 
+# https://leetcode.com/problems/subsets/
 class Solution:
-    # Recursive Approach
-    def __init__(self):
-        self.ans = []
-
-    def helper(self, base: List[int], addon: List[int]) -> None:
-        for index, item in enumerate(addon):
-            new_base = base[:] + [item]
-            self.ans.append(new_base)
-            self.helper(new_base, addon[index + 1 :])
+    def generateSubsets(self, nums: List[int], current: List[int]) -> None:
+        self.ans.append(current)
+        for index, num in enumerate(nums):
+            self.generateSubsets(nums[index + 1 :], current + [num])
 
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        # Subset Size 0
-        self.ans.append([])
+        self.ans = []
 
-        # Subset Size 1
-        for num in nums:
-            self.ans.append([num])
-
-        # Subset Size 2 and more
-        for index, num in enumerate(nums):
-            self.helper([num], nums[index + 1 :])
+        # Size 0 or more
+        self.generateSubsets(nums[:], [])
 
         return self.ans
+
+
+print(Solution().subsets([1, 2, 3]))
+print(Solution().subsets([0]))
