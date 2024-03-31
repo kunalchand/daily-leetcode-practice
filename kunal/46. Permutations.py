c42ae571c429f8dbaa392a024d76ff8a9963586f
Kunal Chand <kunalchand234@gmail.com>
2024-03-31 06:09:19 -0400

    46. Permutations (Code Update)

--- kunal/46. Permutations.py
+++ kunal/46. Permutations.py
@@ -1,23 +1,36 @@
-from typing import List
+import copy
+import heapq
+import math
+import random
+from collections import Counter, defaultdict, deque
+from dataclasses import dataclass
+from functools import cmp_to_key, reduce
+from itertools import zip_longest
+from math import factorial, sqrt
+from typing import Dict, List, Optional, Set, Tuple, Union
 
 
+# https://leetcode.com/problems/permutations/description/
 class Solution:
-    def generateNewAddon(self, index: int, addon: List[int]) -> List[int]:
-        new_addon = addon[:]
-        new_addon.pop(index)
-        return new_addon
-
-    def helper(self, ongoing: List[int], addon: List[int]) -> List[List[int]]:
-        if len(addon) == 0:
-            return [ongoing]
+    # Reduce Options Approach
+    def generatePermutations(self, current: List[int], options: Set) -> None:
+        if len(options) == 0:
+            self.ans.append(current)
+            return
         else:
-            ans = []
-            for index, item in enumerate(addon):
-                permutations = self.helper(ongoing, self.generateNewAddon(index, addon))
-                for permutation in permutations:
-                    ans.append(ongoing + [item] + permutation)
-
-            return ans
+            for option in options.copy():
+                options.remove(option)
+                self.generatePermutations(current + [option], options)
+                options.add(option)
 
     def permute(self, nums: List[int]) -> List[List[int]]:
-        return self.helper([], nums)
+        self.ans = []
+
+        self.generatePermutations([], set(nums))
+
+        return self.ans
+
+
+print(Solution().permute([1, 2, 3]))
+print(Solution().permute([0, 1]))
+print(Solution().permute([1]))
