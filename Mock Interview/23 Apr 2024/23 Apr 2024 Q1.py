930bccb0723dc8865c07bc3b67f39119ba508128
Kunal Chand <kunalchand234@gmail.com>
2024-04-23 22:22:47 -0400

    23 Apr 2024 Code Structure

--- Mock Interview/23 Apr 2024/23 Apr 2024 Q1.py
+++ Mock Interview/23 Apr 2024/23 Apr 2024 Q1.py
@@ -20,3 +20,22 @@ Example 2:
 Input: wall = [[1],[1],[1]]
 Output: 3
 """
+
+import bisect
+import copy
+import heapq
+import math
+import random
+from bisect import bisect, bisect_left, bisect_right, insort, insort_left, insort_right
+from collections import Counter, defaultdict, deque
+from dataclasses import dataclass
+from functools import cmp_to_key, reduce
+from itertools import zip_longest
+from math import ceil, factorial, floor, sqrt
+from typing import Dict, List, Optional, Set, Tuple, Union
+
+
+class Solution:
+    def leastBricks(self, wall: List[List[int]]) -> int:
+        # Write Your Code Here
+        return -1
