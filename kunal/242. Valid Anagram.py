e74b409ed9fff838ea02d98da8f9f030a1afda9d
Kunal Chand <kunalchand234@gmail.com>
2024-05-01 03:49:52 -0400

    242. Valid Anagram (Array Method)

--- kunal/242. Valid Anagram.py
+++ kunal/242. Valid Anagram.py
@@ -10,8 +10,9 @@ from math import factorial, sqrt
 from typing import Dict, List, Optional, Set, Tuple, Union
 
 
+# https://leetcode.com/problems/valid-anagram/
 class Solution:
-    # Sort & Compare Time-O(n logn) Space-O(1)
+    # Sort & Compare, Time-O(n logn) Space-O(1)
     """
     def isAnagram(self, s: str, t: str) -> bool:
         s1 = sorted(s)
@@ -19,15 +20,34 @@ class Solution:
         return s1==s2
     """
 
-    # HashTable Time-O(n) Space-O(n)
+    # HashTable, Time-O(n) Space-O(n)
+    """
     def isAnagram(self, s: str, t: str) -> bool:
         s_map = Counter(s)
         t_map = Counter(t)
-
+        
         if s_map == t_map:
             return True
         else:
             return False
+    """
+
+    # Array, Time-O(n) Space-O(n)
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_array = [0] * 26
+        t_array = [0] * 26
+
+        for s_char in s:
+            s_array[ord(s_char) - ord("a")] += 1
+
+        for t_char in t:
+            t_array[ord(t_char) - ord("a")] += 1
+
+        for index in range(26):
+            if s_array[index] != t_array[index]:
+                return False
+
+        return True
 
 
 print(Solution().isAnagram("anagram", "nagaram"))
