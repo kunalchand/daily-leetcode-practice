51bf7c81fc868b1ed1582306c2295025b90a1daa
Kunal Chand <kunalchand234@gmail.com>
2024-05-09 17:06:20 -0400

    Q3

---
+++ Mock Interview/Next Interview For Rishabh/Q3.py
@@ -0,0 +1,46 @@
+"""
+1249. Minimum Remove to Make Valid Parentheses
+
+Given a string s of '(' , ')' and lowercase English characters.
+
+Your task is to remove the minimum number of parentheses ( '(' or ')', in any positions ) so that the resulting parentheses string is valid and return any valid string.
+
+Formally, a parentheses string is valid if and only if:
+
+It is the empty string, contains only lowercase characters, or
+It can be written as AB (A concatenated with B), where A and B are valid strings, or
+It can be written as (A), where A is a valid string.
+
+Example 1:
+
+Input: s = "lee(t(c)o)de)"
+Output: "lee(t(c)o)de"
+Explanation: "lee(t(co)de)" , "lee(t(c)ode)" would also be accepted.
+
+Example 2:
+N/A
+
+Example 3:
+N/A
+
+Constraints:
+N/A
+"""
+
+import bisect
+import copy
+import heapq
+import math
+import random
+from bisect import bisect, bisect_left, bisect_right, insort, insort_left, insort_right
+from collections import Counter, defaultdict, deque
+from dataclasses import dataclass
+from functools import cmp_to_key, reduce
+from itertools import zip_longest
+from math import ceil, factorial, floor, sqrt
+from typing import Dict, List, Optional, Set, Tuple, Union
+
+
+class Solution:
+    def minRemoveToMakeValid(self, s: str) -> str:
+        pass
