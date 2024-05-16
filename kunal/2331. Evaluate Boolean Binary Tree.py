e4633a1c9c673494c64ff6a24da2b6328f49dfe8
Kunal Chand <kunalchand234@gmail.com>
2024-05-16 04:35:29 -0400

    2331. Evaluate Boolean Binary Tree

---
+++ kunal/2331. Evaluate Boolean Binary Tree.py
@@ -0,0 +1,33 @@
+import bisect
+import copy
+import heapq
+import math
+import random
+from bisect import bisect, bisect_left, bisect_right, insort, insort_left, insort_right
+from collections import Counter, defaultdict, deque
+from dataclasses import dataclass
+from functools import cmp_to_key, reduce
+from itertools import zip_longest
+from math import ceil, factorial, floor, sqrt
+from typing import Dict, List, Optional, Set, Tuple, Union
+
+
+# https://leetcode.com/problems/evaluate-boolean-binary-tree/
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def evaluateTree(self, root: Optional[TreeNode]) -> bool:
+        if root.val == 0:
+            return False
+        elif root.val == 1:
+            return True
+        elif root.val == 2:
+            return self.evaluateTree(root.left) or self.evaluateTree(root.right)
+        elif root.val == 3:
+            return self.evaluateTree(root.left) and self.evaluateTree(root.right)
