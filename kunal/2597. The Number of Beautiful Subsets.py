3cedad2a80b172470e77e44924e3b715ba9316c4
Kunal Chand <kunalchand234@gmail.com>
2024-05-23 03:26:40 -0400

    2597. The Number of Beautiful Subsets (different way to do it)

--- kunal/2597. The Number of Beautiful Subsets.py
+++ kunal/2597. The Number of Beautiful Subsets.py
@@ -14,18 +14,32 @@ from typing import Dict, List, Optional, Set, Tuple, Union
 
 # https://leetcode.com/problems/the-number-of-beautiful-subsets/
 class Solution:
-    # Backtracking, Recursion
+    # Recursion Global Count
+    """
     def generateSubset(self, nums: List[int], k: int, subset: List) -> None:
         for index in range(len(nums)):
-            if (nums[index] - k) not in subset:
+            if (nums[index] - k) not in subset and (nums[index] + k) not in subset:
                 # self.allSubset.append(subset + [nums[index]])
                 self.count += 1
-                self.generateSubset(nums[index + 1 :], k, subset + [nums[index]])
+                self.generateSubset(nums[index+1:], k, subset + [nums[index]])
 
     def beautifulSubsets(self, nums: List[int], k: int) -> int:
         self.count = 0
         # self.allSubset = []
-        nums.sort()
         self.generateSubset(nums, k, [])
         # print(self.allSubset)
         return self.count
+    """
+
+    # Backtracking, Recursion
+    def generateSubset(self, nums: List[int], k: int, subset: List) -> int:
+        count = 0
+        for index in range(len(nums)):
+            if (nums[index] - k) not in subset and (nums[index] + k) not in subset:
+                count += 1 + self.generateSubset(
+                    nums[index + 1 :], k, subset + [nums[index]]
+                )
+        return count
+
+    def beautifulSubsets(self, nums: List[int], k: int) -> int:
+        return self.generateSubset(nums, k, [])
