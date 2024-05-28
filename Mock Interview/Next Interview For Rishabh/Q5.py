3d498735fa0fa312790c85c4865e6469b3b92fa3
Rishabh <rishabhvrm43@gmail.com>
2024-05-28 16:34:16 -0400

    add Q5

--- Mock Interview/Next Interview For Rishabh/Q5.py
+++ Mock Interview/Next Interview For Rishabh/Q5.py
@@ -23,3 +23,39 @@ Example 3:
 Constraints:
     N/A
 """
+
+# 4 * O(rotation) + O(comparison) => O(n ** 2)
+
+# transposing + reversing => rotated()
+# compare
+
+# mat[i][j] <=> mat[j][i]
+
+from typing import List
+class Solution:
+    def transpose_mat(self, mat: List[List[int]]) -> None:
+        for r in range(self.n):
+            for c in range(self.n):
+                if r != c:
+                    mat[r][c], mat[c][r] = mat[c][r], mat[r][c]
+
+
+    def reverse_mat(self, mat: List[List[int]]) -> None:
+        for r in range(self.n):
+            mat[r] = reversed(mat[r])
+
+    def rotate_mat(self, mat: List[List[int]]) -> None:
+        self.transpose_mat(mat)
+        self.reverse_mat(mat)
+
+    def compare_mat(self, mat: List[List[int]], target: List[List[int]]) -> bool:
+        return mat == target
+
+    def findRotation(self, mat: List[List[int]], target: List[List[int]]) -> bool:
+        self.n = len(mat)
+        for _ in range(5):
+            self.rotate_mat(mat)
+            if self.compare_mat(mat, target): 
+                return True
+
+        return False
