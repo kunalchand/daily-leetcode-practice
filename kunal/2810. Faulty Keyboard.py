f2c27776b6f4726e6567509c4c6a976fa02974bf
Kunal Chand <kunalchand234@gmail.com>
2024-05-28 15:59:20 -0400

    2810. Faulty Keyboard (Reduce Reversals)

--- kunal/2810. Faulty Keyboard.py
+++ kunal/2810. Faulty Keyboard.py
@@ -14,6 +14,8 @@ from typing import Dict, List, Optional, Set, Tuple, Union
 
 # https://leetcode.com/problems/faulty-keyboard/
 class Solution:
+    # Brute Force
+    """
     def finalString(self, s: str) -> str:
         ans = ""
         for char in s:
@@ -22,3 +24,34 @@ class Solution:
             else:
                 ans += char
         return ans
+    """
+
+    # Deque, Without Multiple Reversal, Time-O(n) Space-O(n)
+    # Reference: https://leetcode.com/problems/faulty-keyboard/solutions/3871597/o-n-solution-without-reversing-each-time
+    def finalString(self, s: str) -> str:
+        reverseCount = 0
+        dq = deque()
+        addDirection = "right"
+
+        for char in s:
+            if char == "i":
+                reverseCount += 1
+                if addDirection == "right":
+                    addDirection = "left"
+                elif addDirection == "left":
+                    addDirection = "right"
+            else:
+                if addDirection == "right":
+                    dq.append(char)
+                elif addDirection == "left":
+                    dq.appendleft(char)
+
+        ans = ""
+
+        while dq:
+            ans += dq.popleft()
+
+        if reverseCount % 2 == 0:
+            return ans
+        else:
+            return "".join(reversed(ans))
