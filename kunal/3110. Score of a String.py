b02c9815a7719b86f0afe0e05f54ff7bb2062c52
Kunal Chand <kunalchand234@gmail.com>
2024-06-01 12:50:04 -0400

    3110. Score of a String (using pairwise)

--- kunal/3110. Score of a String.py
+++ kunal/3110. Score of a String.py
@@ -7,7 +7,7 @@ from bisect import bisect, bisect_left, bisect_right, insort, insort_left, insor
 from collections import Counter, defaultdict, deque
 from dataclasses import dataclass
 from functools import cmp_to_key, reduce
-from itertools import zip_longest
+from itertools import combinations, pairwise, permutations, zip_longest
 from math import ceil, factorial, floor, sqrt
 from typing import Dict, List, Optional, Set, Tuple, Union
 
@@ -15,4 +15,8 @@ from typing import Dict, List, Optional, Set, Tuple, Union
 # https://leetcode.com/problems/score-of-a-string/
 class Solution:
     def scoreOfString(self, s: str) -> int:
-        return sum(abs(ord(s[i]) - ord(s[i + 1])) for i in range(len(s) - 1))
+        return sum(abs(ord(a) - ord(b)) for a, b in pairwise(s))
+
+
+print(Solution().scoreOfString("hello"))
+print(Solution().scoreOfString("zaz"))
