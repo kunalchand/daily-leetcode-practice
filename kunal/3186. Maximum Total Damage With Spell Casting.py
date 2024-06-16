ad87cd41e75ccba3e59126c7d416e380c5f1681d
Kunal Chand <kunalchand234@gmail.com>
2024-06-16 13:27:55 -0400

    3186 @cache

--- kunal/3186. Maximum Total Damage With Spell Casting.py
+++ kunal/3186. Maximum Total Damage With Spell Casting.py
@@ -6,7 +6,7 @@ import random
 from bisect import bisect, bisect_left, bisect_right, insort, insort_left, insort_right
 from collections import Counter, defaultdict, deque
 from dataclasses import dataclass
-from functools import cmp_to_key, reduce
+from functools import cache, cmp_to_key, lru_cache, reduce
 from itertools import combinations, pairwise, permutations, zip_longest
 from math import ceil, factorial, floor, inf, sqrt
 from typing import Dict, List, Optional, Set, Tuple, Union
@@ -15,6 +15,8 @@ from typing import Dict, List, Optional, Set, Tuple, Union
 # https://leetcode.com/problems/maximum-total-damage-with-spell-casting/
 class Solution:
     # Reference: https://www.twitch.tv/videos/2173278701
+    # Manual Memoization
+    """
     def recursion(self, index: int, oldDamage: int) -> int:
         if (index, oldDamage) in self.memo:
             return self.memo[(index, oldDamage)]
@@ -29,6 +31,33 @@ class Solution:
 
             # Can Damage
             # Damage
+            maxPower = max(self.power[index], self.power[index] + self.recursion(index + 1, self.power[index]))
+            # No Damage
+            maxPower = max(maxPower, self.recursion(index + 1, max(oldDamage, self.power[index] - 3)))
+            # Refer twitch to know why max and how to fix MLE
+
+            self.memo[(index, oldDamage)] = maxPower
+            return self.memo[(index, oldDamage)]
+
+    def maximumTotalDamage(self, power: List[int]) -> int:
+        self.power = sorted(power)
+        self.memo = {}
+        return self.recursion(0, -inf)
+    """
+
+    # Reference: https://www.twitch.tv/videos/2173278701
+    # Automatic Memoization
+    @cache  # or @lru_cache(None)
+    def recursion(self, index: int, oldDamage: int) -> int:
+        if index >= len(self.power):
+            return 0
+        else:
+            # Can't Damage
+            if self.power[index] == oldDamage + 1 or self.power[index] == oldDamage + 2:
+                return self.recursion(index + 1, oldDamage)
+
+            # Can Damage
+            # Damage
             maxPower = max(
                 self.power[index],
                 self.power[index] + self.recursion(index + 1, self.power[index]),
@@ -40,10 +69,8 @@ class Solution:
             )
             # Refer twitch to know why max and how to fix MLE
 
-            self.memo[(index, oldDamage)] = maxPower
-            return self.memo[(index, oldDamage)]
+            return maxPower
 
     def maximumTotalDamage(self, power: List[int]) -> int:
         self.power = sorted(power)
-        self.memo = {}
         return self.recursion(0, -inf)
