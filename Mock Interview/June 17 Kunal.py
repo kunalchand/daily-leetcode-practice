5505f16dd29fed99b57b046c7681c1045dc51924
Rishabh <rishabhvrm43@gmail.com>
2024-06-17 15:14:08 -0700

    add kunal intv questions

---
+++ Mock Interview/June 17 Kunal.py
@@ -0,0 +1,39 @@
+'''
+Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the integers in the range [1, n] that do not appear in nums.
+
+ 
+
+Example 1:
+
+Input: nums = [4,3,2,7,8,2,3,1]
+Output: [5,6]
+Example 2:
+
+
+
+'''
+def fun1(self, nums: List[int]) -> List[int]:
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+
+Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+
+
+'''
+def fun2(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+    pass
