40a3a14fb642df5fdfc9a618b1221855f5ec3d7b
Kunal Chand <kunalchand234@gmail.com>
2024-06-17 17:03:13 -0400

    740. Delete and Earn (1D DP @cache)

--- kunal/740. Delete and Earn.py
+++ kunal/740. Delete and Earn.py
@@ -15,6 +15,8 @@ from typing import Dict, List, Optional, Set, Tuple, Union
 # https://leetcode.com/problems/delete-and-earn/
 # Similar to 3186. Maximum Total Damage With Spell Casting - https://leetcode.com/problems/maximum-total-damage-with-spell-casting/
 class Solution:
+    # 2D DP Top Down Memoization
+    """
     @cache
     def recursion(self, index: int, oldElement: int) -> int:
         if index >= len(self.nums):
@@ -24,19 +26,37 @@ class Solution:
 
             # Earn
             if not self.nums[index] == oldElement + 1:
-                maxPoints = max(
-                    maxPoints,
-                    self.nums[index] + self.recursion(index + 1, self.nums[index]),
-                )
+                maxPoints = max(maxPoints, self.nums[index] + self.recursion(index+1, self.nums[index]))
 
             # No Earn
-            maxPoints = max(
-                maxPoints,
-                self.recursion(index + 1, max(oldElement, self.nums[index] - 2)),
-            )
+            maxPoints = max(maxPoints, self.recursion(index+1, max(oldElement, self.nums[index] - 2)))
 
             return maxPoints
 
     def deleteAndEarn(self, nums: List[int]) -> int:
         self.nums = sorted(nums)
         return self.recursion(0, -inf)
+    """
+
+    # 1D DP Top Down Memoization
+    @cache
+    def recursion(self, index: int) -> int:
+        if index >= len(self.nums):
+            return 0
+        else:
+            # Earn
+            num, freq = self.nums[index]
+            earnPoints = num * freq
+            if index + 1 < len(self.nums) and self.nums[index + 1][0] != num + 1:
+                earnPoints += self.recursion(index + 1)
+            else:
+                earnPoints += self.recursion(index + 2)
+
+            # No Earn
+            notEarnPoints = self.recursion(index + 1)
+
+            return max(earnPoints, notEarnPoints)
+
+    def deleteAndEarn(self, nums: List[int]) -> int:
+        self.nums = sorted(Counter(nums).items())
+        return self.recursion(0)
