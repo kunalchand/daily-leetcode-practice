76989eea63de4dde66f194ec4784fa69d6373339
Rishabh <rishabhvrm43@gmail.com>
2024-06-19 13:10:13 -0700

    add Q1

--- Mock Interview/rishabh-interview/19 Jun 2024/Q1.py
+++ Mock Interview/rishabh-interview/19 Jun 2024/Q1.py
@@ -7,6 +7,23 @@ In one operation, you can change any character of s to any other character.
 
 Return true if you can make s a palindrome after performing 
 exactly one or two operations, or return false otherwise.
+
+1 <= len <= 10 ^ 5
+
+s = "abcd" => "abca" > "acca" > 
+s = "abcd" => "aaa" > T
+
+
+valid:
+    even 
+    odd: only one key should be odd, everything else should be even
+
+    
+  l    r
+dcxZZZZycd
+
+op = 2
+
 """
 
 import bisect
@@ -25,4 +42,23 @@ from typing import Dict, List, Optional, Set, Tuple, Union
 
 class Solution:
     def makePalindrome(self, s: str) -> bool:
-        pass
+        # s = list(s)
+        if len(s) != 1:
+            l, r = 0, len(s) - 1
+            oprs = 2
+
+            while l <= r:
+                if s[l] != s[r]:
+                    # s[l] = s[r]
+                    oprs -= 1
+
+                l, r = l + 1, r - 1
+
+            if oprs < 0:
+                return False
+        return True
+    
+    #    l r
+    # abcdedcba
+        #   lr
+        # fecdef
