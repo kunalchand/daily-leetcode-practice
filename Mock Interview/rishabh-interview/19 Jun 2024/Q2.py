27995d1a3175b8cc6b410cf4a5b43d13dcb35af9
Rishabh <rishabhvrm43@gmail.com>
2024-06-19 14:31:38 -0700

    add Q2

--- Mock Interview/rishabh-interview/19 Jun 2024/Q2.py
+++ Mock Interview/rishabh-interview/19 Jun 2024/Q2.py
@@ -4,7 +4,23 @@ https://leetcode.com/problems/valid-palindrome-ii/
 
 Given a string s, return true if the s can be palindrome 
 after deleting at most one character from it.
+
+abac => t
+abba => t
+
+abca => aba
+
+
+    l         r
+b a c b b b a c b
+
+
+  l r
+a b c a
+
+
 """
+
 
 import bisect
 import copy
@@ -22,4 +38,28 @@ from typing import Dict, List, Optional, Set, Tuple, Union
 
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-        pass
+
+        def is_palindrome(st):
+            return st == st[::-1]
+
+        l, r = 0, len(s) - 1
+
+        while l <= r:
+            if s[l] != s[r]:
+                # skip R
+                l = l
+                r -= 1
+                first_res = is_palindrome(s[l:r+1])
+                r += 1
+
+
+                # skip L
+                l += 1
+                r = r
+                second_res = is_palindrome(s[l:r+1])
+                break
+
+            l, r = l + 1, r - 1    
+
+        return first_res or second_res 
+   
