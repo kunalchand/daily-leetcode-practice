638d32b67f8e71829d8740dc0d516b3319322de1
Rishabh <rishabhvrm43@gmail.com>
2024-06-20 14:43:24 -0700

    add Kunal Interview questions June 20

---
+++ Mock Interview/kunal-interview/20 June 2024/Q1.py
@@ -0,0 +1,21 @@
+'''
+A string is good if there are no repeated characters.
+
+Given a string s​​​​​, return the number of good substrings of length three in s​​​​​​.
+
+Note that if there are multiple occurrences of the same substring, every occurrence should be counted.
+
+A substring is a contiguous sequence of characters in a string.
+
+ 
+
+Example 1:
+
+Input: s = "xyzzaz"
+Output: 1
+Explanation: There are 4 substrings of size 3: "xyz", "yzz", "zza", and "zaz". 
+The only good substring of length 3 is "xyz".
+'''
+class Solution:
+    def countGoodSubstrings(self, s: str) -> int:
+        pass
