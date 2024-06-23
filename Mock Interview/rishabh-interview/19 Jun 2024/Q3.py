8f5e4344f4c9b9543af8c732c16b60d310e7766f
Rishabh <rishabhvrm43@gmail.com>
2024-06-22 22:37:42 -0700

    Rishabh: adding solution

--- Mock Interview/rishabh-interview/19 Jun 2024/Q3.py
+++ Mock Interview/rishabh-interview/19 Jun 2024/Q3.py
@@ -14,9 +14,52 @@ as the number of kilometers due east from the car's initial location.
 Return true if it is possible to pick up and drop off all 
 passengers for all the given trips, or false otherwise.
 
+1 -> 4
+
+(P,1,2) (P,2,3), (P,3,4)
+
+1 
+
 Example:
-    Input: trips = [[2,1,5],[3,3,7]], capacity = 5
+    Input: trips = [[2,1,5],[3,5,7]], capacity = 5 => 
+
+    trips = [ 
+        
+        1 -> [[v, S]]
+        5 -> [[2, E], [3, S]]
+        [3, S] -> v
+        [7, E] -> v
+        
+        ]
+
     Output: true
+
+    trips [A, B, C]
+
+  2       2
+0 1 2 3 4 5 6 7 8 
+      3       3
+        1   1
+
+0 2 2 5 6 4 3 0             
+        
+1 9
+
+1000 * 1000 => 10 ^ 6
+ 
+n -> len(t)
+m -> range
+
+O(n + m)
+O(m)
+
+
+1 6 
+2 7
+3 8
+
+
+
 """
 
 import bisect
@@ -35,4 +78,24 @@ from typing import Dict, List, Optional, Set, Tuple, Union
 
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
-        pass
+        cust_map = defaultdict(list)
+
+        for trip in trips:
+            p, s, e = trip
+            cust_map[s].append([p,"S"])
+            cust_map[e].append([p,"E"])
+        
+        curr_cap = 0
+        for i in range(1001):
+            if i in cust_map:
+
+                for psngr, sep in cust_map[i]:
+                    if sep == "S":
+                        curr_cap += psngr
+                    else:
+                        curr_cap -= psngr
+
+                if curr_cap > capacity:
+                    return False 
+            
+        return True
