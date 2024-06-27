51aad5232c499af307914a981527f2fde69d25d7
Kunal Chand <kunalchand234@gmail.com>
2024-06-27 17:41:26 -0400

    Mock Solution

--- Mock Interview/kunal-interview/27 June 2024/Q1.py
+++ Mock Interview/kunal-interview/27 June 2024/Q1.py
@@ -1,4 +1,4 @@
-'''
+"""
 LC 19
 Given the head of a linked list, remove the nth node from the end of the list and return its head.
 
@@ -8,15 +8,38 @@ Example 1:
 
 Input: head = [1,2,3,4,5], n = 2
 Output: [1,2,3,5]
+[1,2,a b c d e f g h,3, sf s fsf sf sf s f] NONE
+                               A         B
+"""
 
-'''
+
 # Definition for singly-linked list.
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
 
+
 from typing import Optional
+
+# [D,2,3,4,5]
+#  S
+#             F
+
+
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        pass
+        dummyNode = ListNode(-1, head)
+        slow = dummyNode
+        fast = dummyNode
+
+        for _ in range(n):
+            fast = fast.next
+
+        while fast.next:
+            slow = slow.next
+            fast = fast.next
+
+        slow.next = slow.next.next
+
+        return dummyNode.next
--- Mock Interview/kunal-interview/27 June 2024/Q2.py
+++ Mock Interview/kunal-interview/27 June 2024/Q2.py
@@ -1,4 +1,4 @@
-'''
+"""
 LC 203
 
 Given the head of a linked list and an integer val, 
@@ -10,14 +10,32 @@ Example 1:
 
 Input: head = [1,2,6,3,4,5,6], val = 6
 Output: [1,2,3,4,5]
-'''
+"""
+
+
 # Definition for singly-linked list.
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
 
+
 from typing import Optional
+
+# [D,1,3,4,5]
+#          C
+
+
 class Solution:
     def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
-        pass
+        dummyNode = ListNode(-1, head)
+
+        # Perform Deletion
+        current = dummyNode
+        while current and current.next:
+            if current.next.val == val:
+                current.next = current.next.next
+            else:
+                current = current.next
+
+        return dummyNode.next
