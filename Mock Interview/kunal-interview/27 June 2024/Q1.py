fe50966203e181faaf23678bfc047963021fe52c
Rishabh <rishabhvrm43@gmail.com>
2024-06-27 13:55:38 -0700

    add June 27 questions

---
+++ Mock Interview/kunal-interview/27 June 2024/Q1.py
@@ -0,0 +1,22 @@
+'''
+LC 19
+Given the head of a linked list, remove the nth node from the end of the list and return its head.
+
+ 
+
+Example 1:
+
+Input: head = [1,2,3,4,5], n = 2
+Output: [1,2,3,5]
+
+'''
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+from typing import Optional
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        pass
---
+++ Mock Interview/kunal-interview/27 June 2024/Q2.py
@@ -0,0 +1,23 @@
+'''
+LC 203
+
+Given the head of a linked list and an integer val, 
+remove all the nodes of the linked list that has Node.val == val, and return the new head.
+
+
+Example 1:
+
+
+Input: head = [1,2,6,3,4,5,6], val = 6
+Output: [1,2,3,4,5]
+'''
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+from typing import Optional
+class Solution:
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        pass
